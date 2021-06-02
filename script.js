39a7cd3200859e64e43e405532942633e4d53209
titenq <titenq@gmail.com>
2021-06-02 10:16:54 -0300

    include / in figcapiton

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ memesArray.map(meme => {
   memes.innerHTML += `
   <figure>
     <img src="${meme.src}" alt="meme ${meme.id}" width="200">
-    <figcaption>##meme${meme.id}/Texto 1/Texto 2</figcaption>
+    <figcaption>##meme/${meme.id}/Texto 1/Texto 2</figcaption>
   </figure>
   `;
 });
