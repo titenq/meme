a10c9e2a0fc0bd8c9a61df765ee5f218f583f4b7
titenq <titenq@gmail.com>
2021-06-02 00:17:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const memes = document.getElementById('memes');
+
+memesArray.map(meme => {
+  memes.innerHTML += `
+  <figure>
+    <img src="${meme.src}" alt="meme ${meme.id}" width="200">
+    <figcaption>##meme${meme.id}/Texto 1/Texto 2</figcaption>
+  </figure>
+  `;
+});
